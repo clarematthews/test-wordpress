b96abf09b0a00403b4ff5471080337703487aa46
clare <cmatthew@thoughtworks.com>
2016-03-21 11:36:09 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `wp_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_revisr` WRITE;
 /*!40000 ALTER TABLE `wp_revisr` DISABLE KEYS */;
-INSERT INTO `wp_revisr` VALUES (1,'2016-03-21 09:35:39','Successfully created a new repository.','init','clare'),(2,'2016-03-21 09:36:27','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=bcf5672&success=true\">#bcf5672</a> to the local repository.','commit','clare'),(3,'2016-03-21 09:41:56','Successfully pushed 2 commits to origin/master.','push','clare'),(4,'2016-03-21 10:30:52','There was an error committing the changes to the local repository.','error','clare'),(5,'2016-03-21 10:41:32','Error backing up the database.','error','clare');
+INSERT INTO `wp_revisr` VALUES (1,'2016-03-21 09:35:39','Successfully created a new repository.','init','clare'),(2,'2016-03-21 09:36:27','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=bcf5672&success=true\">#bcf5672</a> to the local repository.','commit','clare'),(3,'2016-03-21 09:41:56','Successfully pushed 2 commits to origin/master.','push','clare'),(4,'2016-03-21 10:30:52','There was an error committing the changes to the local repository.','error','clare'),(5,'2016-03-21 10:41:32','Error backing up the database.','error','clare'),(6,'2016-03-21 11:29:00','Successfully backed up the database.','backup','clare');
 /*!40000 ALTER TABLE `wp_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
